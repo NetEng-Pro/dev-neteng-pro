43cd5bc8efd1c52a6513fb40512749c5f5bd5fa0
SunDevil311 <github@sl.neteng.cc>
2025-01-24 21:44:02 -0700

    Switch to GH packages

--- dist/js/app.9eaff430f961b81d1233.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-(self.webpackChunk_neteng_pro_nwpro_web = self.webpackChunk_neteng_pro_nwpro_web || []).push([ [ 524 ], {
-    316: () => {}
-}, e => {
-    var n;
-    n = 316, e(e.s = n);
-} ]);
