bdf3a5824043d1af857382f0ede2d95ea161a8cb
SunDevil311 <github@sl.neteng.cc>
2025-01-24 20:42:58 -0700

    Updates

--- test/test.mjs
+++ test/test.mjs
@@ -1,8 +1,10 @@
-// test/test.js
+// test/test.mjs
+/* SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+   This file is part of Network Engineering Pro */
 
 /* eslint-env mocha */
 
-const assert = require('assert');
+import assert from 'assert';
 
 describe('Array', function () {
   describe('#indexOf()', function () {
