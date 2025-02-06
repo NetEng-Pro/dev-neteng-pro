adaa5c33dd4552b8a3f1a16b6d2589196846c03a
SunDevil311 <github@sl.neteng.cc>
2025-02-05 21:33:55 -0700

    Keep calm and commit

---
+++ dist/js/app.3c45a30a7cfa25c559c4.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(self.webpackChunk_neteng_pro_nwpro_dev =
+  self.webpackChunk_neteng_pro_nwpro_dev || []).push([
+  [524],
+  {
+    428: () => {
+      (void 0).self;
+    },
+  },
+  (e) => {
+    var _;
+    (_ = 428), e((e.s = _));
+  },
+]);
